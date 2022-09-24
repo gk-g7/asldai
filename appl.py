6f7fe7712a82d0140b2199d32d8aae9a36d57b5a
Gokul G <>
2022-09-24 17:58:47 +0530

    heroku depl

--- appl.py
+++ appl.py
@@ -1,13 +1,7 @@
-import uvicorn
 import fastapi
 
 app = fastapi.FastAPI()
 
-
 @app.get('/helloworld')
 def helloworld():
     return {"msg":"Hello World!"}
-
-
-if __name__ == '__main__':
-    uvicorn.run(app)
